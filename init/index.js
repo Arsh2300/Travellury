8cee726e37673886e290926cff500526d32c4633
Amolpreet0171 <amolpreet0171.becse24@chitkara.edu.in>
2025-04-18 22:31:20 +0530

    Update index.js

--- init/index.js
+++ init/index.js
@@ -4,6 +4,7 @@ const mongoose=require("mongoose");
 const initdata=require("./data.js");
 const Listing=require("../models/listing.js");
 
+// Connect to MongoDB using environment variable
 const dbUrl = process.env.ATLASDB_URL;
 console.log(dbUrl);
 
@@ -18,11 +19,15 @@ async function main() {
 }
 
 const initDb=async ()=>{
+    // Clear existing listings from the database
    await Listing.deleteMany({});
+    // Set a fixed owner ID for each listing
    initdata.data= initdata.data.map((obj)=>({
     ...obj,
     owner:"677a30350b2c230769cbed3c"
    }))
+    
+    // Insert the modified data into the database
     Listing.insertMany(initdata.data);
     console.log("data Initialized");
 }
