b5c36099b5897d1dcbc0ffa017165060bdada223
ayush <ayush0188.becse24@chitkara.edu.in>
2025-05-06 14:20:57 +0530

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,3 @@
-
 const User=require("../models/user");
 
 module.exports.renderSignupForm=(req,res)=>{
@@ -7,21 +6,19 @@ module.exports.renderSignupForm=(req,res)=>{
 
 module.exports.signupUser=async(req,res)=>{
     try{
-        // Create and register a new user
         let {username,email,password}=req.body;
         const newUser=new User({username,email});
         const registeredUser= await User.register(newUser,password); 
         console.log(registeredUser);
-        // Automatically log in the user after signup
         req.login(registeredUser,(err)=>{
             if(err){
                 return next(err);
             }
-            req.flash("success","Travellury");
+            req.flash("success","Welcome to BookNest");
             res.redirect("/listings");  
         })
             
-        // Handle errors like duplicate usernames
+        
     }catch(e){
         req.flash("error","Username already exists");
         res.redirect("/signup");
@@ -35,13 +32,12 @@ module.exports.renderLoginForm=async(req,res)=>{
 
 module.exports.loginUser=
 async (req,res)=>{
-req.flash("success","Travellury");
+req.flash("success","Welcome back to BookNest");
 let redirectUrl=res.locals.redirectUrl ||"/listings";
 res.redirect(redirectUrl);
 }
 
 module.exports.logoutUser=(req,res,next)=>{
-    // Log out the user and redirect
     req.logout((err)=>{
         if(err){
            return next(err);
