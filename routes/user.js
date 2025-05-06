88e482149134b8c1503c374f5c6d3d787229c850
Amolpreet0171 <amolpreet0171.becse24@chitkara.edu.in>
2025-05-06 15:18:33 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -4,13 +4,10 @@ const wrapAsync=require("../utils/wrapAsync.js");
 const passport=require("passport");
 const { saveRedirectUrl } = require("../middleware.js");
 const userController = require("../controllers/users.js");
-
 router
     .route("/signup")
     .get(userController.renderSignupForm)
     .post(wrapAsync(userController.signupUser))
-
-
 router
     .route("/login")
     .get(userController.renderLoginForm)
@@ -20,8 +17,5 @@ router
         failureFlash:true,
         }),
         userController.loginUser)
-
 router.get("/logout",userController.logoutUser)
-
-
 module.exports=router;
