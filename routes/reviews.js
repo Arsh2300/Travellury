2283cd40e50f43a7dcec7adae476dfa0fb50617b
Amolpreet0171 <amolpreet0171.becse24@chitkara.edu.in>
2025-05-06 15:17:34 +0530

    Update reviews.js

--- routes/reviews.js
+++ routes/reviews.js
@@ -4,21 +4,15 @@ const wrapAsync=require("../utils/wrapAsync.js");
 const {validateReview, isLoggedin, isOwner, isReviewAuthor}=require("../middleware.js");
 const reviewController=require("../controllers/reviews.js");
 const ExpressError=require("../utils/ExpressError.js");
-
 //reviews
-
 //post review Route
-
 router.post("/",
     isLoggedin,
     validateReview,
     wrapAsync(reviewController.createReview))
-
 //delete review route
-
 router.delete("/:reviewId",
     isLoggedin,
     isReviewAuthor,
     wrapAsync(reviewController.destroyReview))
-
 module.exports=router;
